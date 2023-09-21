3c358a29caf7ce942ada26185ee9458120d38a67
jimtrung <144447608+jimtrung@users.noreply.github.com>
2023-09-21 11:11:47 +0700

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,1 +1,32 @@
-#include 'Vehicle.h'
+#include "Vehicle.h"
+using namespace Vehicle
+
+Vehicle(string mn): modelName(mn) {}
+
+setModelName(string mn) {
+    modelName = mn;
+}
+
+getModelName() {
+    return modelName;
+}
+
+setOwnerName(string on) {
+    ownerName = on;
+}
+
+getOwnerName() {
+    return ownerName;
+}
+
+setLicensePlate(string lp) {
+    licensePlate = lp;
+}
+
+getLicensePlate() {
+    return licensePlate;
+}
+
+run() {
+    cout << modelName << " is running...!\n";
+}
